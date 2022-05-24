452b7f956ff653f6b8f51e7ddcda73e42017b0cf
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-24 16:43:48 +0200

    iteration 2

--- src/App.js
+++ src/App.js
@@ -10,13 +10,19 @@ function App() {
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
       </tr>
     </thead>
     <tbody>
       {contacts.slice(0,5).map((contact) => {
         return (
         <tr key={contact.name}>
-        <td><img src={contact.pictureUrl} alt='contact' width="100px"/></td><td>{contact.name}</td><td>{contact.popularity.toFixed(2)}</td>
+        <td><img src={contact.pictureUrl} alt='contact' width="100px"/></td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity.toFixed(2)}</td>
+        <td>{contact.wonOscar ? '🏆' : '' }</td>
+        <td>{contact.wonEmmy ? '🏆' : '' }</td>
         </tr>
         )
         })}
